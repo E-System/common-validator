f5f911ec14b08cda6ea608a9521007ca35b62699
diman <memphisprogramming@gmail.com>
2017-06-11 21:03:36 +0700

    Update versions

--- src/test/groovy/com/es/lib/validator/ogrn/OGRNValidatorSpec.groovy
+++ src/test/groovy/com/es/lib/validator/ogrn/OGRNValidatorSpec.groovy
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) Extended System - All Rights Reserved
+ * Copyright (c) E-System - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  *
- * Written by Extended System team (https://ext-system.com), 2015
+ * Written by E-System team (https://ext-system.com), 2015
  */
 
 package com.es.lib.validator.ogrn
